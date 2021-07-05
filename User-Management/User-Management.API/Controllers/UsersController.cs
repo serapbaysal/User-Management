e4b3ebd60c1a437dd5a0102d2ac99ef686e3a7ce
serapbaysal <serap.baysal@hotmail.com>
2021-07-05 21:06:12 +0300

    added request functions

---
+++ User-Management/User-Management.API/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User_Management.API.Fake;
+using User_Management.API.Models;
+
+namespace User_Management.API.Controllers
+{
+    [Route("api/[controller]")]  // controller ismi ne ise [controller] yerine gelir
+    public class UsersController:ControllerBase
+    { 
+        
+        private List<User> _users = FakeData.GetUsers(200);
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _users;
+            
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.Id == id);
+            return user;
+        }
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+
+        [HttpPut]
+        public User Put([FromBody]User user)
+        {
+            var editedUser = _users.FirstOrDefault(x => x.Id == user.Id);
+            editedUser.FirstName = user.FirstName;
+            editedUser.LastName = user.LastName;
+            editedUser.Address = user.Address;
+            
+            return user;
+        }
+
+        [HttpDelete] 
+        public void Delete(int id)
+        {
+            var deletedUser = _users.FirstOrDefault(x => x.Id == id);
+            _users.Remove(deletedUser);
+
+        }
+    }
+}
